80183cad010cca2e3f35e29f5877c9d4e55b7417
schorij23 <j_schori@aol.com>
2023-02-20 21:38:56 +0000

    Lab 4

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -19,7 +19,11 @@ public class Lab {
      * @param y second number to be compared.
      * @return x if bool is true. if bool is false, return y.
      */
-    public int ifExample(boolean bool, int x, int y){
-        return 0;
+    public int ifExample(boolean bool, int x, int y) {
+        if (bool == true) {
+            return x;   
+        } else {
+            return y;
+        }
     }
 }
